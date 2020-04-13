7d8fc138bb003e283d12e4c7edfbf9a382e085c3
McEnos <joshua.odiwuor@turnkeyafrica.com>
2020-04-13 11:22:01 +0300

    java Map interface

--- src/com/company/joshua/Main.java
+++ src/com/company/joshua/Main.java
@@ -2,9 +2,8 @@ package com.company.joshua;
 
 import com.company.joshua.generics.Person;
 
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
@@ -12,27 +11,32 @@ public class Main {
 
         Person draperMan = new Person(89, "DraperMan");
         Person peggy = new Person(67, "Peggy");
-
-        Set<Person> people = new HashSet<>();
-        people.add(draperMan);
-        people.add(draperMan);
-        people.add(peggy);
-        people.add(peggy);
+        Person person = new Person(25, "Joshua");
 
 
-        System.out.println("******Using iterators*******************");
-        // Ensures unique values i.e no duplicates
-        //No defined order, else has no get()
+        Map<String, Person> personMap = new HashMap<>();
+        personMap.put(draperMan.getName(), draperMan);
+        personMap.put(peggy.getName(), peggy);
+        personMap.put(person.getName(), person);
 
-        Iterator<Person> iterator = people.iterator();
-        while (iterator.hasNext()) {
-            Person person = iterator.next();
-            System.out.println(person);
+        System.out.println("***********Looping though keys*********************");
+        for (String name : personMap.keySet()) {
+            System.out.println(name);
         }
-        System.out.println("************Using generic loops************");
 
-        for (Person person : people) {
-            System.out.println(person);
+        System.out.println("************************Looping though values using key**********************");
+        for (String name : personMap.keySet()) {
+            System.out.println(personMap.get(name));
+        }
+
+        System.out.println("***********************Looping through values*******************************");
+        for (Person person1 : personMap.values()) {
+            System.out.println(person1);
+        }
+        System.out.println("****Accessing both key and values******************");
+        for (Map.Entry<String, Person> entry : personMap.entrySet()) {
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
         }
 
 
